bb54b9c14bc785c792c0128c5891df34892168da
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.de>
2021-05-19 17:03:45 +0200

    add spectrogram creator class

---
+++ data_preparation/spectrograms.py
@@ -0,0 +1,102 @@
+import librosa
+import math
+import numpy
+import os
+from skimage import io
+import warnings
+
+warnings.filterwarnings('ignore')
+
+
+class SpectrogramCreator():
+    def __init__(self, chunk_length, data_dir):
+        # parameters for spectorgram creation
+        self.chunk_length = chunk_length  # chunk length in seconds
+        self.sampling_rate = 44100  # number of samples per second
+        self.window_length = 1024
+        self.n_fft = 1024
+        self.hop_length = 196
+        self.fmin = 500  # minimum frequency
+        self.fmax = 15000  # maximum frequency
+
+        self.samples_per_chunk = self.sampling_rate * chunk_length
+
+        self.data_dir = data_dir
+
+        self.audio_dir = os.path.join(self.data_dir, "audio")
+        self.label_dir = os.path.join(self.data_dir, "label")
+        self.spectrogram_dir = os.path.join(
+            self.data_dir, "spectrograms", str(self.chunk_length * 1000))
+
+        if not os.path.exists(self.spectrogram_dir):
+            os.mkdir(self.spectrogram_dir)
+
+    def scale_min_max(self, x, min=0.0, max=1.0):
+        x_std = (x - x.min()) / (x.max() - x.min())
+        x_scaled = x_std * (max - min) + min
+        return x_scaled
+
+    def save_spectrogram(self, target_file, spectrogram):
+        # scale amplitude values to range [0, 1]
+        img = self.scale_min_max(spectrogram, 0, 255).astype(numpy.uint8)
+
+        # put low frequencies at the bottom of the image
+        img = numpy.flip(img, axis=0)
+
+        # invert image so that black represents higher amplitudes
+        img = 255-img
+
+        io.imsave(target_file, img)
+
+    def create_spectrograms_from_file(self, audio_file, target_dir):
+
+        # load audio file
+        amplitudes, sr = librosa.load(audio_file, sr=self.sampling_rate)
+
+        audio_length = amplitudes.shape[0]
+
+        number_of_chunks = math.floor(
+            audio_length / self.samples_per_chunk)
+
+        # split audio file in chunks and create one spectrogram per chunk
+        for i in range(number_of_chunks):
+            # get samples of current chunk
+            chunk = amplitudes[i *
+                               self.samples_per_chunk:(i+1)*self.samples_per_chunk]
+
+            # apply short time fourier transformation to extract frequency information from amplitude data
+            mel_spectrogram = librosa.feature.melspectrogram(chunk, sr=self.sampling_rate, hop_length=self.hop_length, n_fft=self.n_fft,
+                                                             win_length=self.window_length, n_mels=112, fmin=self.fmin, fmax=self.fmax)
+
+            # convert power spectrogram to dB-scaled spectrogram
+            mel_spectrogram_dB = librosa.power_to_db(
+                mel_spectrogram, ref=numpy.max)
+
+            file_name = os.path.splitext(os.path.basename(audio_file))[0]
+            target_file = os.path.join(
+                target_dir, "{}-{}.png".format(file_name, i))
+
+            self.save_spectrogram(target_file, mel_spectrogram_dB)
+
+    def create_species_spectrograms(self, species_name):
+        species_name = species_name.replace(" ", "_")
+        species_audio_dir = os.path.join(self.audio_dir, species_name)
+        species_spectrogram_dir = os.path.join(
+            self.spectrogram_dir, species_name)
+
+        if not os.path.exists(species_spectrogram_dir):
+            os.mkdir(species_spectrogram_dir)
+
+        for file in os.listdir(species_audio_dir):
+            audio_file_path = os.path.join(species_audio_dir, file)
+            self.create_spectrograms_from_file(
+                audio_file_path, species_spectrogram_dir)
+
+    def create_species_list_spectrograms(self, species_list):
+        for species_name in species_list:
+            self.create_species_spectrograms(species_name)
+
+    def create_all_spectrograms(self):
+        for file in os.listdir(self.audio_dir):
+            if os.path.isdir(os.path.join(self.audio_dir, file)):
+                self.create_species_spectrograms(file)
