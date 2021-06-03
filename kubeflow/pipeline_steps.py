befc1fc899a09cf3e17f16690849910e2440b622
Max <mpg.goetz@gmail.com>
2021-06-03 15:17:56 +0200

    Requirements update + Refactor

--- kubeflow/pipeline_steps.py
+++ kubeflow/pipeline_steps.py
@@ -2,9 +2,13 @@ from data_preparation import filepaths, downloader
 
 
 class PipelineSteps:
-    def download_xeno_canto_data(self, data_dir, data_path, species_list, maximum_samples_per_class=100, test_size=0.35, min_quality="E", sound_types=None, sexes=None,
-                                 life_stages=None, exclude_special_cases=True, maximum_number_of_background_species=None, verbose=False):
+    def download_xeno_canto_data(self, data_dir, data_path, species_list, maximum_samples_per_class=100, test_size=0.35,
+                                 min_quality="E", sound_types=None, sexes=None,
+                                 life_stages=None, exclude_special_cases=True,
+                                 maximum_number_of_background_species=None, verbose=False):
         path_manager = filepaths.PathManager(data_dir)
-        XCdownloader = downloader.XenoCantoDownloader(path_manager)
-        XCdownloader.create_datasets(species_list, maximum_samples_per_class=100, test_size=0.35, min_quality="E", sound_types=None, sexes=None,
-                                     life_stages=None, exclude_special_cases=True, maximum_number_of_background_species=None, verbose=False)
+        xc_downloader = downloader.XenoCantoDownloader(path_manager)
+        xc_downloader.create_datasets(species_list, maximum_samples_per_class=100, test_size=0.35, min_quality="E",
+                                      sound_types=None, sexes=None,
+                                      life_stages=None, exclude_special_cases=True,
+                                      maximum_number_of_background_species=None, verbose=False)
