20c0d66c7840a62514a40f7715f77a2d23204ee0
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.de>
2021-06-04 14:43:10 +0200

    remove unnecessary parameter from download_xeno_canto_data step

--- kubeflow/pipeline_steps.py
+++ kubeflow/pipeline_steps.py
@@ -2,7 +2,7 @@ from data_preparation import filepaths, downloader
 
 
 class PipelineSteps:
-    def download_xeno_canto_data(self, data_dir, data_path, species_list, maximum_samples_per_class=100, test_size=0.35,
+    def download_xeno_canto_data(self, data_dir, species_list, maximum_samples_per_class=100, test_size=0.35,
                                  min_quality="E", sound_types=None, sexes=None,
                                  life_stages=None, exclude_special_cases=True,
                                  maximum_number_of_background_species=None, verbose=False):
