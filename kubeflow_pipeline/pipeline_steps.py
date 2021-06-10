e94943432a203c202747545f188838be95fc6a98
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.de>
2021-06-10 13:34:00 +0200

    pass pipeline parameters to downloader component

--- kubeflow_pipeline/pipeline_steps.py
+++ kubeflow_pipeline/pipeline_steps.py
@@ -8,10 +8,10 @@ class PipelineSteps:
                                  maximum_number_of_background_species=None, verbose=False):
         path_manager = filepaths.PathManager(target_dir, gcs_path=gcs_path)
         xc_downloader = downloader.XenoCantoDownloader(path_manager)
-        xc_downloader.create_datasets(species_list, maximum_samples_per_class=100, test_size=0.35, min_quality="E",
-                                      sound_types=None, sexes=None,
-                                      life_stages=None, exclude_special_cases=True,
-                                      maximum_number_of_background_species=None, verbose=False)
+        xc_downloader.create_datasets(species_list, maximum_samples_per_class=maximum_samples_per_class, test_size=test_size, min_quality=min_quality,
+                                      sound_types=sound_types, sexes=sexes,
+                                      life_stages=life_stages, exclude_special_cases=exclude_special_cases,
+                                      maximum_number_of_background_species=maximum_number_of_background_species, verbose=verbose)
 
     def create_spectrograms(self, input_dir, target_dir, gcs_path, chunk_length):
         audio_path_manager = filepaths.PathManager(
