34de7369ac03ed3765adc53af0721a7a10d4cc5b
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.de>
2021-06-22 09:15:34 +0200

    integrate training component in pipeline

--- kubeflow_pipeline/compile_pipeline.py
+++ kubeflow_pipeline/compile_pipeline.py
@@ -11,13 +11,16 @@ def compile_pipeline():
     spectrogram_container_op = kfp.components.load_component_from_file(
         os.path.join(os.getcwd(), 'kubeflow_pipeline/spectrogram_component.yaml'))
 
+    training_container_op = kfp.components.load_component_from_file(
+        os.path.join(os.getcwd(), 'kubeflow_pipeline/training_component.yaml'))
+
     def pipeline(gcs_bucket="bird-song-identification", species_list=None, use_nips4b_species_list=True,
                  maximum_samples_per_class=100, test_size=0.35,
                  min_quality="E", sound_types=None, sexes=None,
                  life_stages=None, exclude_special_cases=True,
                  maximum_number_of_background_species=None,
                  clear_audio_cache=False, clear_label_cache=False,
-                 chunk_length=1000, clear_spectrogram_cache=False, verbose_logging=False):
+                 chunk_length=1000, clear_spectrogram_cache=False, batch_size=32, number_epochs=25, verbose_logging=False):
         download_task = download_data_container_op(
             gcs_bucket=gcs_bucket,
             species_list=species_list,
@@ -41,6 +44,14 @@ def compile_pipeline():
             chunk_length=chunk_length,
             clear_spectrogram_cache=clear_spectrogram_cache,
             verbose_logging=verbose_logging
+        )
+
+        training_task = training_container_op(
+            input_path=spectrogram_task.output,
+            gcs_bucket=gcs_bucket,
+            chunk_length=chunk_length,
+            batch_size=batch_size,
+            number_epochs=number_epochs
         )
 
     pipeline_filename = "birdsong_pipeline.zip"
--- kubeflow_pipeline/pipeline_steps.py
+++ kubeflow_pipeline/pipeline_steps.py
@@ -1,4 +1,6 @@
 import logging
+import torch
+import os
 
 from data_preparation import filepaths, downloader, spectrograms
 from training import training, dataset
@@ -12,41 +14,41 @@ from torch.optim import lr_scheduler
 from general.logging import logger
 
 class PipelineSteps:
-    def download_xeno_canto_data(self, gcs_bucket: str, target_dir: str, verbose_logging: bool, **kwargs):
+    def download_xeno_canto_data(self, gcs_bucket: str, output_path: str, verbose_logging: bool, **kwargs):
         if verbose_logging:
             logger.setLevel(logging.VERBOSE)
-        path_manager = filepaths.PathManager(target_dir, gcs_bucket=gcs_bucket)
+        path_manager = filepaths.PathManager(output_path, gcs_bucket=gcs_bucket)
         xc_downloader = downloader.XenoCantoDownloader(path_manager)
         xc_downloader.create_datasets(**kwargs)
 
-    def create_spectrograms(self, input_dir: str, gcs_bucket: str, target_dir: str, chunk_length: int,
+    def create_spectrograms(self, input_path: str, gcs_bucket: str, output_path: str, chunk_length: int,
                             clear_spectrogram_cache: bool = False, verbose_logging: bool = False):
         if verbose_logging:
             logger.setLevel(logging.VERBOSE)
         audio_path_manager = filepaths.PathManager(
-            input_dir, gcs_bucket=gcs_bucket)
+            input_path, gcs_bucket=gcs_bucket)
         spectrogram_path_manager = filepaths.PathManager(
-            target_dir, gcs_bucket=gcs_bucket)
+            output_path, gcs_bucket=gcs_bucket)
         spectrogram_creator = spectrograms.SpectrogramCreator(
             chunk_length, audio_path_manager, spectrogram_path_manager)
 
         spectrogram_creator.create_spectrograms_for_splits(
             splits=["train", "val", "test"], clear_spectrogram_cache=clear_spectrogram_cache)
 
-    def train_model(self, input_dir, gcs_bucket, batch_size=32, num_epochs=25):
-        spectrogram_path_manager = filepaths.PathManager(input_dir, gcs_bucket=gcs_bucket)
+    def train_model(self, input_path: str, gcs_bucket: str, output_path: str, chunk_length: int, batch_size: int = 32, number_epochs: int = 25):
+        spectrogram_path_manager = filepaths.PathManager(input_path, gcs_bucket=gcs_bucket)
         train_set = dataset.XenoCantoSpectrograms(
-            spectrogram_path_manager, chunk_length=1000, split="train")
+            spectrogram_path_manager, chunk_length=chunk_length, split="train")
         train_loader = DataLoader(
             train_set, batch_size=batch_size, shuffle=True, num_workers=0)
 
         val_set = dataset.XenoCantoSpectrograms(
-            spectrogram_path_manager, chunk_length=1000, split="val")
+            spectrogram_path_manager, chunk_length=chunk_length, split="val")
         val_loader = DataLoader(
             val_set, batch_size=batch_size, shuffle=True, num_workers=0)
 
         test_set = dataset.XenoCantoSpectrograms(
-            spectrogram_path_manager, chunk_length=1000, split="test")
+            spectrogram_path_manager, chunk_length=chunk_length, split="test")
         test_loader = DataLoader(
             test_set, batch_size=batch_size, shuffle=True, num_workers=0)
 
@@ -60,6 +62,10 @@ class PipelineSteps:
 
         exp_lr_scheduler = lr_scheduler.StepLR(my_optimizer, step_size=7, gamma=0.1)
 
-        my_model = training.train_model(train_loader, val_loader, test_loader, resnet_model, my_criterion,
-                                        my_optimizer, exp_lr_scheduler, num_epochs)
+        model = training.train_model(train_loader, val_loader, test_loader, resnet_model, my_criterion,
+                                        my_optimizer, exp_lr_scheduler, number_epochs)
 
+
+        filepaths.PathManager.ensure_dir(output_path)
+
+        torch.save(model, os.path.join(output_path, "model.pt"))
