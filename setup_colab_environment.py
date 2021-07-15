41074e2681bdacf20d95a03b33bb6a929f652eb5
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.de>
2021-07-15 13:50:36 +0200

    add setup script for Google Colab

---
+++ setup_colab_environment.py
@@ -0,0 +1,55 @@
+import argparse
+from google.colab import drive
+import os
+import shutil
+import subprocess
+import sys
+
+def setup_colab_environment(data_path: str, branch: str = "main"):
+    git_repo_dir = "/content/BirdSongIdentification"
+
+    try:
+        subprocess.run(["git", "checkout", branch], cwd=git_repo_dir, check=True)
+        subprocess.run(["git", "pull"], cwd=git_repo_dir, check=True)
+        sys.path.append(git_repo_dir)
+    except subprocess.CalledProcessError:
+        raise NameError(f"Could not check out branch {branch} of Git repo")
+
+    try:
+        subprocess.run(["pip3", "install", "-r", "requirements-colab.txt"], cwd=git_repo_dir, check=True)
+        subprocess.run(["pip3", "install", "-r", "google-cloud-logging== 2.5.0"], cwd=git_repo_dir, check=True)
+    except subprocess.CalledProcessError:
+        raise NameError("Could not install requirements")
+
+    print("\n")
+    print("Load training data from Google drive...")
+    print("\n")
+
+    drive.mount('/content/drive', force_remount=True)
+    colab_data_dir = "/content/data"
+    for split in ["train", "test", "val"]:
+        if not os.path.exists(os.path.join(colab_data_dir, f"{split}.zip")):
+            os.makedirs(colab_data_dir, exist_ok=True)
+            shutil.copy(os.path.join(data_path, f"{split}.zip"), colab_data_dir)
+
+        if not os.path.exists(os.path.join(colab_data_dir, split)):
+            try:
+                subprocess.run(["unzip", "-q", os.path.join(colab_data_dir, f"{split}.zip")],
+                               cwd=colab_data_dir, check=True)
+            except subprocess.CalledProcessError:
+                raise NameError(f"Could unzip {split} data set")
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Setup Google Colab environment for model training.")
+    parser.add_argument("data_path", help="Path of the training data in Google drive")
+    parser.add_argument("--branch", required=False, help="Path of the training data in Google drive")
+
+    args = parser.parse_args()
+
+    if not args.branch:
+        args.branch = "main"
+    if not args.imagesize:
+        args.imagesize = 300
+
+    setup_colab_environment(args.data_path, args.branch)
