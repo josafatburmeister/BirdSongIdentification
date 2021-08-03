219ce799a91d076f26df75c2a4de242d95fd2aae
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.de>
2021-08-03 21:26:13 +0200

    rename editable module

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='data_preparation', version='1.0', packages=find_packages(), include_package_data=True)
+setup(name='bird_song_pipeline', version='1.0', packages=find_packages(), include_package_data=True)
