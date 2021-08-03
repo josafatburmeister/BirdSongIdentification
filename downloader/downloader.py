c1a82d6474bc886bdfde460d722a49c7a02128c2
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.de>
2021-08-03 12:31:22 +0200

    add doc strings to Downloader class

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -1,15 +1,29 @@
 import os
+import pandas as pd
 import requests
 import shutil
 from sklearn.model_selection import train_test_split
-from typing import Optional
+from typing import Optional, Tuple
 
 from general.filepaths import PathManager
 from general.logging import logger
 
 class Downloader:
+    """
+    A base class for custom dataset downloaders.
+    """
+
     @staticmethod
-    def train_test_split(labels, test_size: float = 0.4, random_state: int = 12):
+    def train_test_split(labels: pd.DataFrame, test_size: float = 0.4, random_state: int = 12) -> Tuple[
+        pd.DataFrame, pd.DataFrame]:
+        """
+        Splits pandas dataframe into train and test set.
+
+        param: labels: Labels to split
+        param: test_size: Fraction of labels that should be used for test set
+        param: random_state: Random state for data splitting
+        """
+
         train_labels, test_labels = [], []
         try:
             train_labels, test_labels = train_test_split(
@@ -23,6 +37,10 @@ class Downloader:
         return train_labels, test_labels
 
     def __init__(self, path_manager: PathManager):
+        """
+        param: path_manager: PathManager instance that manages the target folder
+        """
+
         self.path = path_manager
 
         # retrieve cached files from google cloud storage
@@ -36,6 +54,14 @@ class Downloader:
             PathManager.empty_dir(self.path.cache_dir)
 
     def download_file(self, url: str, target_file: str, cache_subdir: Optional[str] = None):
+        """
+        Downloads file from given URL and saves it in target path. If cache_dir parameter is set the downloaded file is cached to speedup later download request of the same file.
+
+        param: url: url of file to download
+        param: target_file: path where downloaded file should be stored
+        param: cache_subdir: Name of a subfolder of the cache folder where the downloaded file should be cached. If not existing, the subfolder is created inside the cache folder.
+        """
+
         if cache_subdir:
             cached_file_path = self.path.cached_file_path(cache_subdir, target_file)
         # check if file is in cache
