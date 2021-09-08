f5e63eacfc45708e5c9d7565489807f7a5cfbe9d
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.de>
2021-09-08 21:15:48 +0200

    add docstrings to Logger and ProgressBar classes

--- general/logging.py
+++ general/logging.py
@@ -7,6 +7,10 @@ from tqdm.notebook import tqdm, tqdm_notebook
 
 
 class Logger:
+    """
+    Class that holds a global logger instance.
+    """
+
     logger = verboselogs.VerboseLogger("Birdsong-Logger")
     logger.propagate = False
     # print logs to console
@@ -18,12 +22,26 @@ logger = Logger.logger
 
 
 class ProgressBar:
+    """
+    Class for logging progress in both notebook and non-notebook environments. In notebook environments a tqdm progress
+    bar is used for progress visualization. In non-notebook environments plain text lines are output for progress logging.
+    """
+
     def __init__(self,
                  total: int = 0,
                  sequence: Optional[Iterable] = None,
                  desc: str = "",
                  position: int = 0,
                  is_pipeline_run: bool = False) -> None:
+        """
+
+        Args:
+            total: Number of steps until the progress bar is finished.
+            sequence: Itarable for which the progress bar is to be incremented at each iteration.
+            desc: Descriptive text to be displayed next to the progress bar.
+            position: Start position of the progress bar.
+            is_pipeline_run: Whether the progress bar is used in a non-notebook execution environment.
+        """
         self.is_pipeline_run = is_pipeline_run
         self.sequence = sequence
         if self.is_pipeline_run:
@@ -37,6 +55,12 @@ class ProgressBar:
                 raise NameError("Either total or sequence parameter must be set")
 
     def iterable(self) -> Union[Optional[Iterable], Type[tqdm_notebook]]:
+        """
+        Creates an Iterable from the progress bar.
+
+        Returns:
+            Iterable wrapper of the progress bar that iterates over the underlying sequence.
+        """
         if self.sequence is None:
             raise NameError("No sequence provided")
         if self.is_pipeline_run:
@@ -45,10 +69,28 @@ class ProgressBar:
             return self.tqdm
 
     def update(self, n: int = 1) -> None:
+        """
+        Updates the progress bar.
+
+        Args:
+            n: Number of steps by which the progress bar should be increased.
+
+        Returns:
+            None
+        """
         if not self.is_pipeline_run:
             self.tqdm.update(n)
 
     def write(self, text: str) -> None:
+        """
+        Logs text below the progress bar.
+
+        Args:
+            text: Text to be logged.
+
+        Returns:
+            None
+        """
         if self.is_pipeline_run:
             logger.info(text)
         else:
